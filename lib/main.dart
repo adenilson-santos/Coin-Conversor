d0d6b4cffef9352a24c52426d989b344d1fa106e
Adenilson Santos <adenilson.santos@gocase.com.br>
2019-12-18 10:21:39 -0300

    showing details

--- lib/main.dart
+++ lib/main.dart
@@ -1,103 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
-void main() => runApp(MyApp());
+const hgApi = 'https://api.hgbrasil.com/finance';
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main () async {
+  runApp(MaterialApp(
+    home: Home(),
+    theme: ThemeData(
+      hintColor: Colors.lightBlue,
+      primaryColor: Colors.white
+    )
+  ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(hgApi);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final realController  = TextEditingController();
+  final euroController  = TextEditingController();
+  final dolarController = TextEditingController();
+  double dolar, euro;
+
+  void _realChanged (String text) {
+    double real = double.parse(text);
+    dolarController.text = (real/dolar).toStringAsFixed(2);
+    euroController.text = (real/euro).toStringAsFixed(2);
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  void _dolarChanged (String text) {
+    print('dolar');
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _euroChanged (String text) {
+    print('euro');
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Colors.lightBlue,
+        title: Center(child: Text('\$ Coin Conversor \$', style: TextStyle(color: Colors.white),), ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      body: FutureBuilder<Map>(
+        future: getData(),
+        builder: (context, snapshot) {
+          switch(snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(child: Text('Loading data...', style: TextStyle(fontSize: 25.0), textAlign: TextAlign.center));
+            default:
+              if(snapshot.hasError) {
+                return Center(child: Text('Error to load data...', style: TextStyle(fontSize: 25.0), textAlign: TextAlign.center));
+              } else {
+                dolar = snapshot.data['results']['currencies']['USD']['buy'];
+                dolar = snapshot.data['results']['currencies']['EUR']['buy'];
+
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(15.0),
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: <Widget>[
+                        Icon(Icons.monetization_on, size: 120.0, color: Colors.lightBlue),
+                        Divider(),
+                        buildTextField('Reais', 'R\$ ', realController, _realChanged),
+                        Divider(),
+                        buildTextField('Dolars', 'US\$ ', dolarController, _dolarChanged),
+                        Divider(),
+                        buildTextField('Euros', '€ ', euroController, _euroChanged)
+                      ],
+                    ),
+                );
+              }
+          }
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+Widget buildTextField (String label, String prefix, TextEditingController controller, Function changedAction) {
+  return TextField(
+    controller: controller,
+    decoration: InputDecoration(
+      labelText: label,
+      labelStyle: TextStyle(color: Colors.lightBlue),
+      border: OutlineInputBorder(),
+      prefixText: prefix,
+    ),
+    style: TextStyle(
+      color: Colors.lightBlue
+    ),
+    onChanged: changedAction,
+    keyboardType: TextInputType.number,
+  );
+}
+
+// theme: ThemeData(
+//   inputDecorationTheme: InputDecorationTheme(
+//     enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white))
+//   )
+// )
